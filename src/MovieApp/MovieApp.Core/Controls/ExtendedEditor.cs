800cefaa7711bd5e8f89f2a889aabf705a212ebf
Arnaud Maichac <arnaud.maichac@outlook.com>
2018-01-17 20:54:37 +0100

    step-03-solution

---
+++ src/MovieApp/MovieApp.Core/Controls/ExtendedEditor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MovieApp.Core.Controls
+{
+    public class ExtendedEditor : Editor
+    {
+        public ExtendedEditor()
+        {
+            this.TextChanged += (sender, args) =>
+            {
+                if (Device.RuntimePlatform == Device.Android)
+                {
+                    this.InvalidateMeasure();
+                }
+            };
+        }
+    }
+}
