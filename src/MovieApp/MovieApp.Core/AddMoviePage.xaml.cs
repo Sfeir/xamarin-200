65c2f06fec7740c2c0b6b34c78405f076ed27c7f
Arnaud Maichac <arnaud.maichac@outlook.com>
2018-01-17 22:03:43 +0100

    step-08-solution

--- src/MovieApp/MovieApp.Core/AddMoviePage.xaml.cs
+++ src/MovieApp/MovieApp.Core/AddMoviePage.xaml.cs
@@ -9,7 +9,6 @@ using Xamarin.Forms.Xaml;
 
 namespace MovieApp.Core
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddMoviePage : ContentPage
     {
         private Movie movie;
