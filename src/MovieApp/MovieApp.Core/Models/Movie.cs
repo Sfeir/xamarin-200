1e419a1ebf7d88f01a48e762fce873337c661b43
Arnaud Maichac <arnaud.maichac@outlook.com>
2018-01-17 20:49:34 +0100

    step-02

---
+++ src/MovieApp/MovieApp.Core/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.Core.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Overview { get; set; }
+        
+        public DateTime ReleaseDate { get; set; }
+    }
+}
