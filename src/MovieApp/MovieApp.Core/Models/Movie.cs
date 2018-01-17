8c96a0b6aaad3ac6c77b4bbe9343103c08394157
Arnaud Maichac <arnaud.maichac@outlook.com>
2018-01-17 22:00:21 +0100

    step-07

--- src/MovieApp/MovieApp.Core/Models/Movie.cs
+++ src/MovieApp/MovieApp.Core/Models/Movie.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace MovieApp.Core.Models
 {
     public class Movie : INotifyPropertyChanged
     {
+        private const string baseImageUrl = "http://image.tmdb.org/t/p/w320";
+
         public int Id { get; set; }
 
         private string title;
@@ -26,7 +29,13 @@ namespace MovieApp.Core.Models
 
         public string Overview { get; set; }
 
+        [JsonProperty("release_date")]
         public DateTime ReleaseDate { get; set; }
+
+        [JsonProperty("poster_path")]
+        public string PosterPath { get; set; }
+
+        public string Image => baseImageUrl + PosterPath;
 
         public override string ToString()
         {
