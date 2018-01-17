ae2842e286acb2831a29b66280d62e1963099034
Arnaud Maichac <arnaud.maichac@outlook.com>
2018-01-17 20:57:09 +0100

    step-04-solution

--- src/MovieApp/MovieApp.Core/AddMoviePage.xaml.cs
+++ src/MovieApp/MovieApp.Core/AddMoviePage.xaml.cs
@@ -16,14 +16,12 @@ namespace MovieApp.Core
         {
             InitializeComponent();
 
-            BindingContext = new Movie();
-
-            this.TitleEntry.Text = "The Dark Knight";
-
-            this.OverviewEditor.Text =
-                "Dans ce nouveau volet, Batman augmente les mises dans sa guerre contre le crime. Avec l'appui du lieutenant de police Jim Gordon et du procureur de Gotham, Harvey Dent, Batman vise à éradiquer le crime organisé qui pullule dans la ville. Leur association est très efficace mais elle sera bientôt bouleversée par le chaos déclenché par un criminel extraordinaire que les citoyens de Gotham connaissent sous le nom de Joker.";
-
-            this.ReleaseDatePicker.Date = new DateTime(2008, 08, 10);
+            BindingContext = new Movie
+            {
+                Title = "The Dark Knight",
+                Overview = "Dans ce nouveau volet, Batman augmente les mises dans sa guerre contre le crime. Avec l'appui du lieutenant de police Jim Gordon et du procureur de Gotham, Harvey Dent, Batman vise à éradiquer le crime organisé qui pullule dans la ville. Leur association est très efficace mais elle sera bientôt bouleversée par le chaos déclenché par un criminel extraordinaire que les citoyens de Gotham connaissent sous le nom de Joker.",
+                ReleaseDate = new DateTime(2008, 08, 10)
+            };
         }
     }
 }
