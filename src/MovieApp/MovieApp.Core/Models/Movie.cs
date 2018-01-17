55b067896931e137920d8b6e2fb1b31d29a28031
Arnaud Maichac <arnaud.maichac@outlook.com>
2018-01-17 21:01:56 +0100

    step-05-solution

--- src/MovieApp/MovieApp.Core/Models/Movie.cs
+++ src/MovieApp/MovieApp.Core/Models/Movie.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MovieApp.Core.Models
 {
-    public class Movie
+    public class Movie : INotifyPropertyChanged
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public string Overview { get; set; }
-        
+
         public DateTime ReleaseDate { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
