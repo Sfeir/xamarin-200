1abafdbf22f9a2a4d3bc7f0078ed3f084fa7bb6e
arnaudmaichac <arnaud.maichac@outlook.com>
2019-02-03 14:07:29 +0100

    step-06

---
+++ src/MovieApp/MovieApp.Core/Store/MovieRepository.cs
@@ -0,0 +1,25 @@
+﻿using MovieApp.Core.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace MovieApp.Core.Store
+{
+    public class MovieRepository
+    {
+        public IList<Movie> Movies { get; }
+
+        public MovieRepository()
+        {
+            var currentAssembly = GetType().GetTypeInfo().Assembly;
+            var stream = currentAssembly.GetManifestResourceStream("MovieApp.Core.Data.movies.json");
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                JObject moviesSearch = JObject.Parse(reader.ReadToEnd());
+                Movies = JsonConvert.DeserializeObject<List<Movie>>(moviesSearch["results"].ToString());
+            }
+        }
+    }
+}
